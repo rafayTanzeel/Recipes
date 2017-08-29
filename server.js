ed362841292e9825fc6dd979690cd6cff781f83d
Rafay <rafaytanzeel@gmail.com>
2017-08-29 19:30:31 -0400

    readjusted port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
