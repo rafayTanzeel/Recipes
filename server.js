75a18c8eb472083d69c7992799c7a0a45be433a3
Rafay <rafaytanzeel@gmail.com>
2017-08-29 22:39:14 -0400

    replaced html with pug

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 const baseurlDisplay = () => {
   const baseurl = 'http://localhost:' + PORT + '/';
@@ -15,4 +17,23 @@ const baseurlDisplay = () => {
 
 app.listen(PORT, (err) => {
   (err) ? winston.error(err) : baseurlDisplay();
+});
+
+
+app.get('/', (req, res) =>{
+  res.render('index', {
+    title: 'Recipes',
+  });
+});
+
+app.get('/contact', (req, res) =>{
+  res.render('contact', {
+    title: 'Contact',
+  });
+});
+
+app.get('/topPicks', (req, res) =>{
+  res.render('topPicks', {
+    title: 'Top Recipes',
+  });
 });
